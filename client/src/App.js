5dcb8b4f599893d7e1d9eb0fd555f324dc25abb5
rumothy <rumothy@yahoo.com>
2020-02-22 08:11:54 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
 
 class App extends Component {
+  state = {};
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pushing to Heroku</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Nav />
+        <Jumbotron />
       </div>
     );
   }
