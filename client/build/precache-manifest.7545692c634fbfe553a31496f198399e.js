6a3a53f0c12efad0606df5cb0d1d95dce2eb1039
rumothy <rumothy@yahoo.com>
2020-02-22 03:22:31 -0500

    Initial commit

---
+++ client/build/precache-manifest.7545692c634fbfe553a31496f198399e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "787a19d03783725dbe0f",
+    "url": "/static/css/main.8ac5035c.chunk.css"
+  },
+  {
+    "revision": "787a19d03783725dbe0f",
+    "url": "/static/js/main.0e85d70b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3070b4aec8da0e6e7adb",
+    "url": "/static/js/2.a9a10943.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "97cd6f3b215f0e737e6c95cd62997eae",
+    "url": "/index.html"
+  }
+];
