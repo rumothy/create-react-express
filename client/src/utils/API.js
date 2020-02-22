0909aefe0d42b4db6b762b811dc66513901d8475
rumothy <rumothy@yahoo.com>
2020-02-22 18:48:30 -0500

    Add test function to API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,9 @@ import axios from "axios";
 export default {
   getRecipes: function(query) {
     return axios.get("/api/recipes", { params: { q: query } });
+  },
+
+  test: function(arg) {
+    console.log(`Hello ${arg}`);
   }
 };
