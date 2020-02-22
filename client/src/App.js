2b2ac463db474e25b7551e2a8e47bc5a928d9ed7
rumothy <rumothy@yahoo.com>
2020-02-22 18:23:07 -0500

    Create app structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
 import Input from "./components/Input";
+import Button from "./components/Button";
+import { Container, Row, Col } from "./components/Grid";
 
 class App extends Component {
   state = {};
@@ -11,6 +13,29 @@ class App extends Component {
       <div>
         <Nav />
         <Jumbotron />
+        <Container>
+          <Row>
+            <Col>
+              <form>
+                <Container>
+                  <Row>
+                    <Col>
+                      <Input />
+                    </Col>
+                    <Col>
+                      <Button>Search</Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h1>Render Recipes Here</h1>
+            </Col>
+          </Row>
+        </Container>
         <Input />
       </div>
     );
