6763bc88ff0ab700b4347b025e30434438b2d77f
rumothy <rumothy@yahoo.com>
2020-02-22 19:43:17 -0500

    Implement link to recipe puppy about

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,6 +5,13 @@ function Jumbotron() {
   return (
     <div className="jumbotron text-center">
       <h1>React Recipes</h1>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://www.recipepuppy.com/about/us"
+      >
+        Powered by Recipe Puppy
+      </a>
     </div>
   );
 }
